484bdb6e6eed5d945971c5b2e0cdeea5c2205979
Violetta <343454640@mail.ru>
2022-07-18 22:51:34 +0300

    Removed all comands from the view

--- AggressivenessOfWaterAndGround/Model/Water/AggressivenessOfCl.cs
+++ AggressivenessOfWaterAndGround/Model/Water/AggressivenessOfCl.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AggressivenessOfWaterAndGround.WaterData.Water
+namespace AggressivenessOfWaterAndGround.Model.Water
 {
     internal static class AggressivenessOfCl
     {
