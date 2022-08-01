70cb4904e98bf8f7ce25b201ffd1db4f0e062ef3
xxMesseRxx <343454640@mail.ru>
2022-08-01 23:45:08 +0300

    Improve View, added some events, which check so that user don't input extra characters it textbox

--- AggressivenessOfWaterAndGround/MainWindow.xaml.cs
+++ AggressivenessOfWaterAndGround/MainWindow.xaml.cs
@@ -27,5 +27,63 @@ namespace AggressivenessOfWaterAndGround
 
             DataContext = new MainViewModel();
         }
+
+        //Logic for better working of View
+
+        //Checks that the user enters an integer in the textbox, so user can't even input incorrect char
+        private void CheckInt_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
+        }
+
+        //Checks that the user enters an double in the textbox, so user can't even input incorrect char
+        private void CheckDouble_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, 0))
+            {
+                //Checking that user inputed a only one "."
+                if ((e.Text == ".") && !((TextBox)sender).Text.Contains(".") && ((TextBox)sender).Text.Length != 0)
+                {
+                    return;
+                }
+
+                e.Handled = true;
+            }
+
+        }
+
+        //Checks so that user don't input "space" and transmits focus to next element by "Enter"
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+
+            if (e.Key == Key.Enter)
+            {
+                TraversalRequest tRequest = new TraversalRequest(FocusNavigationDirection.Next);
+                UIElement keyboardFocus = Keyboard.FocusedElement as UIElement;
+
+                if (keyboardFocus != null)
+                {
+                    keyboardFocus.MoveFocus(tRequest);
+                }
+
+                e.Handled = true;
+            }
+        }
+
+        //Selects all text in textBox when it got focus
+        private void SelectAllWhen_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBox)
+            {
+                ((TextBox)sender).SelectAll();
+            }
+        }
     }
 }
