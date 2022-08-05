219ee85f734237d7c9953a8bebd2083dae4f5756
xxMesseRxx(working comp.) <343454640@mail.ru>
2022-08-05 15:31:01 +0300

    Rebuilt AggressivenessOfCl for INotifyPropertyChanged inteface

--- AggressivenessOfWaterAndGround/Model/DegreeOfAggres.cs
+++ AggressivenessOfWaterAndGround/Model/DegreeOfAggres.cs
@@ -9,6 +9,7 @@ namespace AggressivenessOfWaterAndGround.Model
     static internal class DegreeOfAggres
     {
         internal const string NonAggres = "Неагрессивная";
+        internal const string Aggres = "Агрессивная";
         internal const string MildlyAggres = "Слабоагрессивная";
         internal const string MediumAggres = "Среднеагрессивная";
         internal const string HighlyAggres = "Сильноагрессивная";
--- AggressivenessOfWaterAndGround/Model/Water/AggressivenessOfCl.cs
+++ AggressivenessOfWaterAndGround/Model/Water/AggressivenessOfCl.cs
@@ -3,205 +3,236 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace AggressivenessOfWaterAndGround.Model.Water
 {
-    internal static class AggressivenessOfCl
+    internal class AggressivenessOfCl : INotifyPropertyChanged
     {
-        public static uint AmountCL { private get; set; }
-        public static bool CoefFiltratMoreThan01 { private get; set; }
+        private uint _amountCl;
+        private bool _coefFiltratMoreThan01;
 
-        const string NonAggres = "Неагрессивная";
-        const string Aggres = "Агрессивная";
+        public uint AmountCl
+        {
+            get { return _amountCl; }
+            set 
+            { 
+                _amountCl = value; 
+                OnPropertyChanged("AmountCl");
+                AllCementPropertyChanged();
+            }
+        }
+        public bool CoefFiltratMoreThan01
+        {
+            get { return _coefFiltratMoreThan01; }
+            set 
+            { 
+                _coefFiltratMoreThan01 = value;
+                OnPropertyChanged("CoefFiltratMoreThan01");
+                AllCementPropertyChanged();
+            }
+        }
 
-        public static string Cement_20_W6_W8
+        public string Cement_20_W6_W8
         {
             get
             {
                 if (CoefFiltratMoreThan01)
                 {
-                    if (AmountCL > 500)
-                        return Aggres;
+                    if (AmountCl > 500)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
                 else
                 {
-                    if (AmountCL > 1150)
-                        return Aggres;
+                    if (AmountCl > 1150)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
             }
         }
-        public static string Cement_20_W10_W14
+        public string Cement_20_W10_W14
         {
             get
             {
                 if (CoefFiltratMoreThan01)
                 {
-                    if (AmountCL > 1300)
-                        return Aggres;
+                    if (AmountCl > 1300)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
                 else
                 {
-                    if (AmountCL > 3000)
-                        return Aggres;
+                    if (AmountCl > 3000)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
             }
         }
-        public static string Cement_20_W16_W20
+        public string Cement_20_W16_W20
         {
             get
             {
                 if (CoefFiltratMoreThan01)
                 {
-                    if (AmountCL > 4100)
-                        return Aggres;
+                    if (AmountCl > 4100)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
                 else
                 {
-                    if (AmountCL > 5000)
-                        return Aggres;
+                    if (AmountCl > 5000)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
             }
         }
-        public static string Cement_30_W6_W8
+        public string Cement_30_W6_W8
         {
             get
             {
                 if (CoefFiltratMoreThan01)
                 {
-                    if (AmountCL > 700)
-                        return Aggres;
+                    if (AmountCl > 700)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
                 else
                 {
-                    if (AmountCL > 1400)
-                        return Aggres;
+                    if (AmountCl > 1400)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
             }
         }
-        public static string Cement_30_W10_W14
+        public string Cement_30_W10_W14
         {
             get
             {
                 if (CoefFiltratMoreThan01)
                 {
-                    if (AmountCL > 1850)
-                        return Aggres;
+                    if (AmountCl > 1850)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
                 else
                 {
-                    if (AmountCL > 3700)
-                        return Aggres;
+                    if (AmountCl > 3700)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
             }
         }
-        public static string Cement_30_W16_W20
+        public string Cement_30_W16_W20
         {
             get
             {
                 if (CoefFiltratMoreThan01)
                 {
-                    if (AmountCL > 8300)
-                        return Aggres;
+                    if (AmountCl > 8300)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
                 else
                 {
-                    if (AmountCL > 9500)
-                        return Aggres;
+                    if (AmountCl > 9500)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
             }
         }
-        public static string Cement_50_W6_W8
+        public string Cement_50_W6_W8
         {
             get
             {
                 if (CoefFiltratMoreThan01)
                 {
-                    if (AmountCL > 1000)
-                        return Aggres;
+                    if (AmountCl > 1000)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
                 else
                 {
-                    if (AmountCL > 1750)
-                        return Aggres;
+                    if (AmountCl > 1750)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
             }
         }
-        public static string Cement_50_W10_W14
+        public string Cement_50_W10_W14
         {
             get
             {
                 if (CoefFiltratMoreThan01)
                 {
-                    if (AmountCL > 2700)
-                        return Aggres;
+                    if (AmountCl > 2700)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
                 else
                 {
-                    if (AmountCL > 4700)
-                        return Aggres;
+                    if (AmountCl > 4700)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
             }
         }
-        public static string Cement_50_W16_W20
+        public string Cement_50_W16_W20
         {
             get
             {
                 if (CoefFiltratMoreThan01)
                 {
-                    if (AmountCL > 18000)
-                        return Aggres;
+                    if (AmountCl > 18000)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
                 else
                 {
-                    if (AmountCL > 20000)
-                        return Aggres;
+                    if (AmountCl > 20000)
+                        return DegreeOfAggres.Aggres;
                     else
-                        return NonAggres;
+                        return DegreeOfAggres.NonAggres;
                 }
             }
         }
-        public static string[] GetAllCement()
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
-            string[] Set = {
-                             Cement_20_W6_W8, Cement_20_W10_W14, Cement_20_W16_W20,
-                             Cement_30_W6_W8, Cement_30_W10_W14, Cement_30_W16_W20,
-                             Cement_50_W6_W8, Cement_50_W10_W14, Cement_50_W16_W20
-                           };
-            return Set;
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private void AllCementPropertyChanged()
+        {
+            OnPropertyChanged("Cement_20_W6_W8");
+            OnPropertyChanged("Cement_20_W10_W14");
+            OnPropertyChanged("Cement_20_W16_W20");
+            OnPropertyChanged("Cement_30_W6_W8");
+            OnPropertyChanged("Cement_30_W10_W14");
+            OnPropertyChanged("Cement_30_W16_W20");
+            OnPropertyChanged("Cement_50_W6_W8");
+            OnPropertyChanged("Cement_50_W10_W14");
+            OnPropertyChanged("Cement_50_W16_W20");
         }
     }
 }
