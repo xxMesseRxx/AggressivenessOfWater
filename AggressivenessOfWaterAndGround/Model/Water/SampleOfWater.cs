932f78f9605de4e60601107cd56e3be4c8a4d0b0
xxMesseRxx(working comp.) <343454640@mail.ru>
2022-08-09 10:59:33 +0300

    New class - SampleOfWater was created, in process adding fields

---
+++ AggressivenessOfWaterAndGround/Model/Water/SampleOfWater.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggressivenessOfWaterAndGround.Model.Water
+{
+    internal class SampleOfWater
+    {
+        private static string _objectName;
+        private string _boreholeNumber;
+        private string _samplingDepth;
+        private uint _concentrationOfNaAndKInMgPerLiter;
+        private uint _concentrationOfMgInMgPerLiter;
+        private uint _concentrationOfCaInMgPerLiter;
+        private uint _concentrationOfClInMgPerLiter;
+        private uint _concentrationOfSoInMgPerLiter;
+        private uint _concentrationOfHcoInMgPerLiter;
+        private uint _concentrationOfNoInMgPerLiter;
+        private uint _dryResidueInMgPerLiter;
+        private double _concentrationOfNaAndKInMgEquivalents;
+        private double _concentrationOfMgInMgEquivalents;
+        private double _concentrationOfCaInMgEquivalents;
+        private double _concentrationOfClInMgEquivalents;
+        private double _concentrationOfSoInMgEquivalents;
+        private double _concentrationOfHcoInMgEquivalents;
+        private double _generalWaterHardnessInMgEquivalents;
+    }
+}
