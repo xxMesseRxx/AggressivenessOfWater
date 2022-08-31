67c68751fc982e83869f33d8d81e0ff008affdfa
xxMesseRxx <343454640@mail.ru>
2022-08-31 22:41:46 +0300

    New class "WorkingObject" was created

--- AggressivenessOfWaterAndGround/Model/Water/SampleOfWater.cs
+++ AggressivenessOfWaterAndGround/Model/Water/SampleOfWater.cs
@@ -1,30 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AggressivenessOfWaterAndGround.Model.Water
 {
-    internal class SampleOfWater
+    internal class SampleOfWater : INotifyPropertyChanged
     {
-        private static string _objectName;
         private string _boreholeNumber;
-        private string _samplingDepth;
-        private uint _concentrationOfNaAndKInMgPerLiter;
-        private uint _concentrationOfMgInMgPerLiter;
-        private uint _concentrationOfCaInMgPerLiter;
-        private uint _concentrationOfClInMgPerLiter;
-        private uint _concentrationOfSoInMgPerLiter;
-        private uint _concentrationOfHcoInMgPerLiter;
-        private uint _concentrationOfNoInMgPerLiter;
-        private uint _dryResidueInMgPerLiter;
-        private double _concentrationOfNaAndKInMgEquivalents;
-        private double _concentrationOfMgInMgEquivalents;
-        private double _concentrationOfCaInMgEquivalents;
-        private double _concentrationOfClInMgEquivalents;
-        private double _concentrationOfSoInMgEquivalents;
-        private double _concentrationOfHcoInMgEquivalents;
-        private double _generalWaterHardnessInMgEquivalents;
+        private double _samplingDepth;
+        private uint _concentrationOfNaAndKInMilligramsPerLiter;
+        private uint _concentrationOfMgInMilligramsPerLiter;
+        private uint _concentrationOfCaInMilligramsPerLiter;
+        private uint _concentrationOfClInMilligramsPerLiter;
+        private uint _concentrationOfSoInMilligramsPerLiter;
+        private uint _concentrationOfHcoInMilligramsPerLiter;
+        private uint _concentrationOfNoInMilligramsPerLiter;
+        private uint _dryResidueInMilligramsPerLiter;
+        private double _concentrationOfNaAndKInMilligramsEquivalents;
+        private double _concentrationOfMgInMilligramsEquivalents;
+        private double _concentrationOfCaInMilligramsEquivalents;
+        private double _concentrationOfClInMilligramsEquivalents;
+        private double _concentrationOfSoInMilligramsEquivalents;
+        private double _concentrationOfHcoInMilligramsEquivalents;
+        private double _generalWaterHardnessInMilligramsEquivalents;
+        private double _permanentWaterHardnessInMilligramsEquivalents;
+        private double _carbonateWaterHardnessInMilligramsEquivalents;
+        private double _pHindicator;
+
+        public string BoreholeNumber
+        {
+            get { return _boreholeNumber; }
+            set
+            {
+                _boreholeNumber = value;
+                OnPropertyChanged("BoreholeNumber");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
