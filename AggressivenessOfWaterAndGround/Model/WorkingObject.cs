cfec789d15389ba2b3cff083557925e5b42989d2
xxMesseRxx <343454640@mail.ru>
2022-09-09 21:23:16 +0300

    Some properties were added to class WorkingObject

--- AggressivenessOfWaterAndGround/Model/WorkingObject.cs
+++ AggressivenessOfWaterAndGround/Model/WorkingObject.cs
@@ -10,6 +10,38 @@ namespace AggressivenessOfWaterAndGround.Model
 {
     internal class WorkingObject : INotifyPropertyChanged
     {
+        private string _code;
+        private string _name;
+        private string _archiveNumber; 
+
+        public string ObjectCode
+        {
+            get { return _code; }
+            set
+            { 
+                _code = value;
+                OnPropertyChanged("ObjectCode");
+            }
+        }
+        public string ObjectName
+        {
+            get { return _name; }
+            set 
+            { 
+                _name = value;
+                OnPropertyChanged("ObjectName");
+            }
+        }
+        public string ArchiveNumber
+        {
+            get { return _archiveNumber; }
+            set
+            { 
+                _archiveNumber = value;
+                OnPropertyChanged("ArchiveNumber");
+            }
+        }
+
 
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string prop = "")
